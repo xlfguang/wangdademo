15bf9f2cbd0728165d3d6390d2aa3a1374163c43
Guang <d405107912@gmail.com>
2026-09-01 13:48:27 +0800

    feat：set prod base url

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://3x2157121c.qicp.vip:59924',
+        target: 'http://192.168.2.48:22301',
         changeOrigin: true,
       },
     },
